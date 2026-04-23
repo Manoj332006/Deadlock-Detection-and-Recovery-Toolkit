d5d50bb9910c7af92d6c7c306a5ed87634192650
Manoj332006 <manojmanoj56443@gmail.com>
2026-04-23 18:24:26 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Deadlock Detection Toolkit")
+
+st.title("Deadlock Detection and Recovery Toolkit")
+
+st.write("Welcome! Use the sidebar to navigate.")
+
+st.sidebar.success("Select a page above")
