d18d7f4423064c8854f113988f1ba69aa7c4af9f
Manoj332006 <manojmanoj56443@gmail.com>
2026-04-23 18:27:13 +0530

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Deadlock Detection Toolkit")
-
-st.title("Deadlock Detection and Recovery Toolkit")
-
-st.write("Welcome! Use the sidebar to navigate.")
-
-st.sidebar.success("Select a page above")
